50e2f853873e3259070a87fef291c76657ee4741
SherzodAxadov2 <sherzodaxadov69@gmail.com>
2023-04-10 15:59:01 +0500

    added parallax

--- js/main.js
+++ js/main.js
@@ -3,3 +3,28 @@ const menuNavigation = document.querySelector(".navigation-primary--mobile");
 const showMenu = () => {
   menuNavigation.classList.toggle("open");
 };
+
+// Parallax
+document.addEventListener("mousemove", parallax);
+function parallax(e) {
+  document.querySelectorAll(".destination__img").forEach(function (move) {
+    let moving__value = move.getAttribute("data-value");
+    console.log(moving__value);
+    let x = e.clientX / moving__value;
+    let y = e.clientY / moving__value;
+
+    move.style.transform = "translateX(" + x + "px) translateY(" + y + "px)";
+  });
+}
+
+document.addEventListener("mousemove", parallaxRotate);
+function parallaxRotate(e) {
+  document.querySelectorAll(".tech__img").forEach(function (move) {
+    let moving__value = move.getAttribute("data-rotate");
+    console.log(moving__value);
+    let x = e.clientX / moving__value;
+    let y = e.clientY / moving__value;
+
+    move.style.transform = "rotateX(" + x + "deg) rotateY(" + y + "deg)";
+  });
+}
